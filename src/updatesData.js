87fbe99461f5bd8345b2a9d2c7413aafd16ee90a
Daniel S <danielsanchezvel@hotmail.com>
2025-06-27 16:10:05 -0500

    holi

--- src/updatesData.js
+++ src/updatesData.js
@@ -3,6 +3,21 @@ export const updatesData = {
     "June 2025": {
       "Week of June 17–23": [
         {
+          title: "💻  Branded Mobile App promo",
+          description: 
+            "kicked off on 6/23, offering existing Heroes 3 months free",
+          link: "https://kajabi.atlassian.net/wiki/x/L4DgtQ",
+        },
+        {
+          title: "📃 Account Data Deletion for Churned Accounts",
+          description:
+            "We are going to begin deleting unnecessary / The dates have been updated!",
+          link: " https://kajabi.atlassian.net/wiki/x/JQCVp",
+        },
+        
+      ],
+      "Week of June 17–23": [
+        {
           title: "💻 Partner Developer Sites",
           description:
             "Sandbox Sites to show a potential signup what is capable on Kajabi/what their business could look like",
@@ -43,24 +58,32 @@ export const updatesData = {
       ],
     },
     "May 2025": {
+      "Week of May 26": [
+        {
+          title:
+            "💻 Template Store FAQs and Terms & Conditions Important Update",
+          description:
+            "We will not be providing refunds for templates. They are final sale.",
+          link: "https://templates.kajabi.com/pages/purchasing-terms",
+        },
+        {
+          title: "🏷️ James Wedmore Partnership 2025",
+          description:
+            "James will then be offering members of the free training our 3 for $99 deal, which is for new Kajabi signups only.",
+          link: "https://kajabi.atlassian.net/wiki/x/NwDVsg",
+        },
+      ],
       "Week of May 19-26": [
         {
-          title: "💻 Partner Developer Sites",
-          description:
-            "Sandbox Sites to show a potential signup what is capable on Kajabi/what their business could look like",
-          link: "https://kajabi.atlassian.net/wiki/spaces/CXHOME/pages/2904358996",
+          title: "💸 Kajabi Cart: PayPal",
+          description: "support for PayPal was released to 100% of accounts",
+          link: "https://kajabi.atlassian.net/wiki/x/v4DaoQ",
         },
         {
-          title: "📃 Community Custom Page Picker",
+          title: "🏷️ Heroes located in India",
           description:
-            "An improvement to pick any page from your website for embebeding to your Community - Not launched yet",
-          link: "https://help.kajabi.com/hc/en-us/articles/9959624056219-How-to-Add-a-Custom-Page-to-Your-Community",
-        },
-        {
-          title: "💰 Community Upsell Banner",
-          description:
-            "Customizable Offer Banner within Kajabi Communities. This feature allows Heroes to promote their offers directly in the community’s right sidebar",
-          link: "https://help.kajabi.com/hc/en-us/articles/38055529679643-How-to-Add-Offers-in-Kajabi-Communities",
+            "starting Jun 1, 2025 , we will be charging GST on their Kajabi subscriptions",
+          link: "https://kajabi.atlassian.net/wiki/x/AYAAsw",
         },
       ],
     },
@@ -89,6 +112,29 @@ export const updatesData = {
         },
       ],
     },
+    "May 2025": {
+      "Week of May 26": [
+        {
+          title: "🛠️ Support SOP for Partner revamp and Cliff Complaints",
+          description:
+            "New SOP has been launched, it has new macros and guidelines",
+          link: "https://kajabi.slack.com/archives/C03SRUCEPFF/p1748573315946139",
+        },
+      ],
+      "Week of May 19-26": [
+        {
+          title: "💸 Fin email bot", 
+          description: " the replacement for Forethought and our Kai bot, was turned on.", 
+          link: "https://kajabi.slack.com/archives/C02RVLC5V/p1747850400166279",
+        },
+        {
+          title: "Update regarding ChargeDesk and Refunds",
+          description:
+            "starting Jun 1, 2025 , we will be charging GST on their Kajabi subscriptions",
+          link: "https://kajabi.slack.com/archives/C03SRUCEPFF/p1747756267107729",
+        },
+    },
+    
   },
   Internal: {
     "June 2025": {
